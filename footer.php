26dac54dd91f4eefd4b28888003b6dd3910f4958
Ijal Fauzi <ijaliffauzi@gmail.com>
2023-05-24 09:45:45 +0700

    Updated footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,62 @@
+        <div class="site-widget page-section">
+            <div class="wrapper">
+                <div class="row">
+                    <div class="row__large-6">
+                        <div class="widget-1">
+                            <div class="widget-content">
+                                <h3 class="headline headline--widget">Powered by</h3>
+                                <a href="https://kp3.co.id" title="PT Karya Prima Putera Perkasa"><img src="assets/images/kp3.webp" alt="PT Karya Prima Putera Perkasa"></a>
+                            </div>
+                            <div class="widget-content">
+                                <h3 class="haedline headline--widget">Follow Us</h3>
+                                <div class="site-widget__socials">
+                                    <a title="Facebook" href="https://facebook.com/profile.php?id=100092313803048"><img src="assets/images/icons/facebook.webp" alt="Facebook"></a>
+                                    <a title="Instagram" href="https://instagram.com/healthings.kp3"><img src="assets/images/icons/instagram.webp" alt="Instagram"></a>
+                                    <a title="Linkedin" href="https://linkedin.com/showcase/healthings-id/"><img src="assets/images/icons/linkedin.webp" alt="Linkedin"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row__large-6">
+                        <div class="widget-3">
+                            <div class="widget-content widget-content--last-widget">
+                                <h3 class="headline headline--widget">Contact Us</h3>
+                                <ul>
+                                    <li class="address">
+                                        <a title="Temukan di Google Maps" href="https://g.page/kapetiga?share">
+                                            <img src="assets/images/icons/map.webp" alt="Alamat">
+                                            <div class="text-adress">
+                                                Jl. Kebagusan Raya No. 192, Kebagusan, Pasar Minggu, Jakarta Selatan 12520
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="phone">
+                                        <a title="Telepon Kami" href="tel:02178838951">
+                                            <img src="assets/images/icons/phone.webp" alt="Telepon">
+                                            <div class="text-phone">Telp: +62-21-78838951</div>
+                                        </a>
+                                    </li>
+                                    <li class="email">
+                                        <a title="Email Kami" href="mailto:marketing@kp3.co.id">
+                                            <img src="assets/images/icons/envelope.webp" alt="Email">
+                                            <div class="text-email">Email: marketing@kp3.co.id</div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer class="site-footer">
+            <div class="wrapper">
+                <p class="site-footer__text">
+                    © 2023 <a title="PT Karya Prima Putera Perkasa" href="https://kp3.co.id">PT Karya Prima Putera Perkasa</a>. <span>All Rights Reserved.</span>
+                </p>
+            </div>
+        </footer>
         <?php wp_footer(); ?>
     </body>
 </html>
